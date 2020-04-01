8a9364154acad2cab283e868ac32466ef4aa409d
Matthew <matthew.t.mckenney@gmail.com>
2020-04-01 00:00:26 -0600

    go program that reads floats from a file and sums the numbers

--- go-work/src/github.com/headfirstgo/average/main.go
+++ go-work/src/github.com/headfirstgo/average/main.go
@@ -1,13 +1,21 @@
 // average calculates the average of several numbers
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/headfirstgo/datafile"
+	"log"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
 	}
-	fmt.Println(sum)
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
