1dcb606fc530f28819f70e50d7c727e67334e4b4
Matthew <matthew.t.mckenney@gmail.com>
2020-04-07 22:51:48 -0600

    go program practice

---
+++ go-work/src/github.com/headfirstgo/responseSize/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func responseSize(url string) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(len(body))
+
+}
+
+func main() {
+	responseSize("https://golang.org/")
+	responseSize("https://golang.org/doc")
+}
